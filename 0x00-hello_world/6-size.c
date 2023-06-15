2c86336ed3d57bacf47fbce3ac71500ffd95e41a
youssef <elomaryoucef@gmail.com>
2023-06-15 16:45:48 -0700

    print size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,7 +19,7 @@ int main(void)
 	printf("Size of char: %lu byte(s)\n", sizeof(c));
 	printf("Size of int: %lu byte(s)\n", sizeof(i));
 	printf("Size of long int: %lu byte(s)\n", sizeof(li));
-	printf("Size of long long int: %lu byte(s)\n", sizeof(long (lli));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(lli));
 	printf("Size of float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
