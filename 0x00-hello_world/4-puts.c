3d89d0e80f36d28f2f9dbf4c86a2bdb298f62435
youssef <elomaryoucef@gmail.com>
2023-06-15 11:56:24 -0700

    printf fun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,1 @@
-#include<stdio.h>
 
-/**
- * main - Entry point
- *
- * Description : a C program that  prints with puts function
- *
- * return : Always 0 (Success)
-*/
-
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Description: a c program that print with function printf
+ *
+ * return: always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
