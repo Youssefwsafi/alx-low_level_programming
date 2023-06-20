296545130e142d01cffc6b749c0360710b548cde
youssef <elomaryoucef@gmail.com>
2023-06-20 14:22:14 -0700

    add a to b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adding numbe to other
+ *
+ * @a: output 1
+ * @b: output 2
+ * Return: sum.
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
