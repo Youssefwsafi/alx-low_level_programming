96ccef2409046be03d5052b8cf09eb35db9231cd
youssef <elomaryoucef@gmail.com>
2023-06-21 07:46:33 -0700

    fibonacci04

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define LARGEST 10000000000
 
 /**
  * main - entry point
@@ -8,30 +9,30 @@
 
 int main(void)
 {
-	unsigned long int a1 = 0, b1 = 1, f2 = 0, b2 = 2;
+	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
 	unsigned long int hold1, hold2, hold3;
 	int count;
 
-	printf("%lu, %lu, ", b1, b2);
+	printf("%lu, %lu, ", bk1, bk2);
 	for (count = 2; count < 98; count++)
 	{
-	if (b1 + b2 > LARGEST  f2 > 0  a1 > 0)
-	{
-	hold1 = (b1 + b2) / LARGEST;
-	hold2 = (b1 + b2) % LARGEST;
-	hold3 = a1 + f2 + hold1;
-	a1 = f2, f2 = hold3;
-	b1 = b2, b2 = hold2;
-	printf("%lu%010lu", f2, b2);
-	}
-	else
-	{
-	hold2 = b1 + b2;
-	b1 = b2, b2 = hold2;
-	printf("%lu", b2);
-	}
-	if (count != 97)
-	printf(", ");
+		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
+		{
+			hold1 = (bk1 + bk2) / LARGEST;
+			hold2 = (bk1 + bk2) % LARGEST;
+			hold3 = fr1 + fr2 + hold1;
+			fr1 = fr2, fr2 = hold3;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu%010lu", fr2, bk2);
+		}
+		else
+		{
+			hold2 = bk1 + bk2;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu", bk2);
+		}
+		if (count != 97)
+			printf(", ");
 	}
 	printf("\n");
 	return (0);
