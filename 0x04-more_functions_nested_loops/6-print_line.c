068fa36b985183b32b355e8cb769d5e4be7afd12
youssef <elomaryoucef@gmail.com>
2023-06-22 12:17:31 -0700

    print _

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - fucnction print __
+ *
+ * @n: take output
+ *
+ *
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+		_putchar('_');
+	else
+		_putchar('\n');
+}
