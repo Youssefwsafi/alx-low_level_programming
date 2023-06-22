7a1525d364d1213de054ca546847601f1954dff3
youssef <elomaryoucef@gmail.com>
2023-06-22 10:19:11 -0700

    mult two digit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ ** mul - function that multi
+ * two digits
+ *
+ * @a: first input
+ * @b: second input
+ * Return: results
+ **/
+int mul(int a, int b)
+{
+	return (a * b);
+}
