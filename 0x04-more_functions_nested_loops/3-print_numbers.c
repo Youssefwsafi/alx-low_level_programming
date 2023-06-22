33b317c4a7aced5941a1f3ae839fb694775e3cd0
youssef <elomaryoucef@gmail.com>
2023-06-22 10:53:00 -0700

    print digit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ ** print_numbers - function that prints
+ * digits to 9
+ *
+ *
+ * Return: void.
+ **/
+void print_numbers(void)
+{
+	int digit = 0;
+
+
+	while (digit <= 9)
+	{
+		_putchar(digit + '0');
+		digit++;
+	}
+
+	_putchar('\n');
+	return;
+}
