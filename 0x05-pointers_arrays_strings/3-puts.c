d34d161c24a3841fa4ecf8613b0f05b604915a4b
youssef <elomaryoucef@gmail.com>
2023-06-26 09:08:02 -0700

    yep

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints string
+ * with a new line to stdout
+ *
+ * @str: a string pointer
+ **/
+
+void _puts(char *str)
+}
+	int puts(const char *s);
+}
