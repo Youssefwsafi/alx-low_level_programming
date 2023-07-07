62044e669247751e83e5bb78f0323f0bec424122
youssef <elomaryoucef@gmail.com>
2023-07-07 08:25:24 -0700

    yep

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,15 @@
 #include "main.h"
 
+/**
+* main - entry point
+* Description: print kevin mitnick.
+* Return: always 0.
+*/
+
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	char *str = "\"At the end of the day, my goal was to"
+	"be the best hacker\"\n\t- Kevin Mitnick";
+	puts(str);
+	return (0);
 }
