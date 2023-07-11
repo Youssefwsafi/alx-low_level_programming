18bd4fabc7c86ed2841d89da1e6472775cd579d2
youssef <elomaryoucef@gmail.com>
2023-07-11 08:21:31 -0700

    nop

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,14 +17,14 @@ char *_strdup(char *str)
 	if (str == NULL)
 	return (NULL);
 
-	for (size = 0; str[size] != '\0'; size++)
+	for (; str[size] != '\0'; size++)
 
 	m = malloc(size * sizeof(*str) + 1);
 
 	if (m == 0)
 	return (NULL);
 	{
-		for (i = 0; i < size; i++)
+		for (; i < size; i++)
 			m[i] = str[i];
 	}
 	return (m);
