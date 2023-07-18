464ec5b11c11d41065a88c061d1939d872a8762c
youssef <elomaryoucef@gmail.com>
2023-07-18 13:28:09 +0100

    yep

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "dog.h"
 
 /**
@@ -18,16 +17,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->age = age;
 	d->owner = owner;
 }
-/**
- * main - entry point
- * Return: always 0
-*/
 
-int main(void)
-{
-	struct dog my_dog;
-
-	init_dog(&my_dog, "Poppy", 3.5, "Bob");
-	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-	return (0);
-}
